be51e11328b686c007df3fb00d4ac448e91099e6
Vyralsyn <Vyralsyn@proton.me>
2025-03-03 07:56:38 +1300

    Added boilerplate React

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>VyralMind React Client</h1>
+      <p>Let's build something awesome! 🚀</p>
+    </div>
+  );
+}
+
+export default App;
